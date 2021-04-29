d4771767f666da19b095a5d53fb0833a3fe02ec5
Magnus <krummelur@gmail.com>
2021-04-29 14:59:26 +0200

    builds lib with --lib

--- reimplementation/unsafe_slist/src/lib.rs
+++ reimplementation/unsafe_slist/src/lib.rs
@@ -2,55 +2,49 @@
  * Fascilitates the language agnostic test suite
  */
 
-use std::sync::Mutex;
-use lazy_static::lazy_static;
-use std::collections::HashMap;
-//Use the crate containing the version to test-
-use singly_linked_list_unsafe::singly_linked_list_unsafe_array as list_impl;
-use list_impl::LinkedList;
-use list_impl::Node;
 
+mod my_library {
 
-lazy_static! { static ref LISTS: Mutex<HashMap<i32, LinkedList<i32>>> = Mutex::new(HashMap::new());}
-
-pub fn test() {
-    let vector = vec!(1);
-    let _test_list = list_impl::linked_list_from(vector);
-}
-
-pub fn init_list(identifier: i32) {
-    LISTS.lock().unwrap().insert(identifier, LinkedList::new());
-}
-
-pub fn insert_head(identifier: i32, value: i32) -> i32 {
-    let mut list = LISTS.lock().unwrap();
-    match list.get_mut(&identifier) {
-        None => -1,
-        Some(_val) => {
-            let list = list.get_mut(&identifier);
-            list.unwrap().insert_head(value);
-            0
+    use std::sync::Mutex;
+    use lazy_static::lazy_static;
+    //Use the crate containing the version to test-
+    use singly_linked_list_unsafe::singly_linked_list_unsafe_array as list_impl;
+    use list_impl::LinkedList;
+    
+    lazy_static! { static ref LISTS: Mutex<Vec<LinkedList<i32>>> = Mutex::new(Vec::new());}
+    
+    #[no_mangle]
+    pub extern "C" fn init_list() -> i32 {
+        match LISTS.lock() {
+            Ok(mut list) => {
+                list.push(LinkedList::new());
+                -1 + list.len() as i32
+            },
+            Err(_) => -1
         }
     }
-}
-
-pub fn insert_after(identifier: i32, index: usize, value: i32) -> i32 {
-    let mut list = LISTS.lock().unwrap();
-    let list = list.get_mut(&identifier);
-    if list.is_some() {
-        let list  = list.unwrap();
-        match list.node_at_index(index) {
-            None => -1, 
-            Some(node) => {
-                list.insert_after(node, value);
-                0
-            }
+    
+    #[no_mangle]
+    pub extern "C" fn insert_head(identifier: u32, value: i32) -> i32 {
+        let mut list = LISTS.lock().unwrap();
+        if let Some(val) = list.get_mut(identifier as usize) {
+            val.insert_head(value);
+            return 0
         }
-    } else {
+        -1
+    }
+    
+    #[no_mangle]
+    pub extern "C" fn insert_after(identifier: u32, index: usize, value: i32) -> i32 {
+        let mut list = LISTS.lock().unwrap();
+        if let Some(list) = list.get_mut(identifier as usize) {
+            if let Ok(Some(node)) = list.node_at_index(index) {        
+                return match list.insert_after(node, value) {
+                    Ok(_) => 0,
+                    _ => -1
+                }
+            } 
+        }
         -1
     }
 }
-
-//extern crate singly_linked_list_unsafe;
-//use singly_linked_list_unsafe::*;
-
