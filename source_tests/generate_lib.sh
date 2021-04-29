f638df8493eb7db2804458b2dea1226723b5a747
Kali <krummelur@gmail.com>
2021-04-29 14:45:38 -0400

    create lib when copying resources

--- source_tests/generate_lib.sh
+++ source_tests/generate_lib.sh
@@ -4,6 +4,7 @@ RUST_SOURCE_PATH="../reimplementation/unsafe_slist"
 RUST_HEADER_PATH="rust_lib.h"
 RUST_LIB_PATH="target/debug/libsingly_linked_list_rust.a"
 RUST_DEBUG_PATH="target/debug/libsingly_linked_list_rust.d"
+mkdir lib
 cd ${RUST_SOURCE_PATH}
 echo "Building library"
 ./gen_lib_bindings.sh 1>/dev/null
@@ -12,4 +13,4 @@ echo "Copying resources"
 cp ${RUST_SOURCE_PATH}/${RUST_LIB_PATH} lib/
 cp ${RUST_SOURCE_PATH}/${RUST_DEBUG_PATH} lib/
 cp ${RUST_SOURCE_PATH}/${RUST_HEADER_PATH} test/lib_impl/
-echo "Done."
+echo "Done"
