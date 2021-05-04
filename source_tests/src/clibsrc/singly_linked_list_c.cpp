65792497bad671c3ab81254343e7dca83f62b42c
Kali <krummelur@gmail.com>
2021-05-04 06:25:38 -0400

    added files for c lib

---
+++ source_tests/src/clibsrc/singly_linked_list_c.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kali on 5/4/21.
+//
+
+#include "singly_linked_list_c.h"
---
+++ source_tests/src/clibsrc/singly_linked_list_c.h
@@ -0,0 +1,14 @@
+//
+// Created by kali on 5/4/21.
+//
+
+#ifndef PROJECT_SINGLY_LINKED_LIST_C_H
+#define PROJECT_SINGLY_LINKED_LIST_C_H
+
+
+class singly_linked_list_c {
+
+};
+
+
+#endif //PROJECT_SINGLY_LINKED_LIST_C_H
