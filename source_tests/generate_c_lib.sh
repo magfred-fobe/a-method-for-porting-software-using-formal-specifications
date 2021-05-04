1675291aa1a7e9188cd719de036d81e94a7d6d95
Kali <krummelur@gmail.com>
2021-05-04 06:55:16 -0400

    Cmake builds the C static lib dependency

--- source_tests/generate_c_lib.sh
+++ source_tests/generate_c_lib.sh
@@ -2,12 +2,15 @@
 WORKING_DIR=$(pwd)
 C_SOURCE_PATH="src/clibsrc/"
 C_SOURCE_BASE_FILENAME="singly_linked_list_c"
+C_LIB_FILENAME="libsingly_linked_list_c.a"
 INTERMEDIATE_OUTPUT_DIR="tmp"
 mkdir -p lib
 cd ${C_SOURCE_PATH}
 mkdir -p ${INTERMEDIATE_OUTPUT_DIR}
 echo "Building c library"
 g++ -c ${C_SOURCE_BASE_FILENAME}.cpp
-ar rvs ${C_SOURCE_BASE_FILENAME}.a ${C_SOURCE_BASE_FILENAME}.o
+ar rvs ${C_LIB_FILENAME} ${C_SOURCE_BASE_FILENAME}.o
+mv ${C_LIB_FILENAME} ../../lib/
+rm ${C_SOURCE_BASE_FILENAME}.o
 cd ${WORKING_DIR}
 echo "Done"
